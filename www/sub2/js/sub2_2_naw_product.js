44e1cfc9f84c5f0e7d468e5138669fd5dfed354b
rlaskarb <rlaskarb20@gmail.com>
2025-07-17 00:12:02 +0900

    7/17

--- www/sub2/js/sub2_2_naw_product.js
+++ www/sub2/js/sub2_2_naw_product.js
@@ -14,7 +14,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const productList = document.createElement("ul");
   productList.className = "new_product_list";
 
+  //데이터 배열을 순서대로읽으며 각상품에 대한 카드 생성
   newProductsData.forEach((productData) => {
     const card = cardTemplate.content.cloneNode(true);
   });
+
+  const image = cardTemplate.querySelector(".new_product_image");
+  image.src = newProductsData(".new_product_image");
+  image.src = productData.imgSrc;
+  image.alt = productData.pName;
+
+  card.querySelector(".new_product_name").textContent = productData.pName;
+  card.querySelector(".new_product_comment").textContent = `분류:
+      ${productData.pType}`;
+  card.querySelector(".product-calorie").textContent = `열량:
+     ${productData.calorie}`;
+  card.querySelector(".product-description").textContent = productData.dCse;
+  productList.appendChild(card);
+  productContainer.appendChild(productList);
 });
