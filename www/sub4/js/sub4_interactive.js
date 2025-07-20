069e62f9df14df96143eb4874995dada0cdb2cf1
김남규 <rlaskarb20@gmail.com>
2025-07-21 08:50:12 +0900

    sub4_interactive.js 업데이트

--- www/sub4/js/sub4_interactive.js
+++ www/sub4/js/sub4_interactive.js
@@ -1,16 +1,18 @@
-// DOM이 모두 로드된 후에 스크립트가 실행되도록 합니다.
+
 document.addEventListener("DOMContentLoaded", function () {
-  // 필요한 요소들을 모두 선택합니다.
+  
   const guide_container = document.getElementById("introduction_guide");
+
   const nav_items = document.querySelectorAll(".start_up_guide_menu li");
+
   const content_items = document.querySelectorAll(".guide_content");
 
-  // 각 탭(nav_items)에 클릭 이벤트를 추가합니다.
+  
   nav_items.forEach(function (nav_item) {
     nav_item.addEventListener("click", function (event) {
       event.preventDefault(); // a 태그의 기본 동작(페이지 이동)을 막습니다.
 
-      // 클릭된 탭에서 data_guide 값을 가져옵니다. (예: "1", "2", "3")
+   
       const target_guide = this.getAttribute("data_guide");
 
       // 1. 모든 요소에서 'active' 클래스 제거 (초기화)
