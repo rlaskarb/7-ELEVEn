d05abf26111a49ef9aab00056eee44574096e69e
rlaskarb <rlaskarb20@gmail.com>
2025-07-27 02:18:32 +0900

    7/27

--- www/main_js/new_meun_slider.js
+++ www/main_js/new_meun_slider.js
@@ -20,16 +20,16 @@ $(document).ready(function () {
       image: "./sub2/images/content2/new3.jpg",
       productName: "크림에 빠진 롤",
       company: "7-ELEVEn",
-      description: "크림에 빠진 롤 삼총사! 납작복숭아/메론/얼그레이레몬롤 ",
-      mdReview: "한 번 빠지면 출구 없는 크림에 빠진 롤!",
+      description: "크림에 빠진 롤 삼총사! 납작복숭아 / 메론 / 얼그레이레몬롤 ",
+      mdReview: "한 번 빠지면 출구 없는 크림 롤!",
       price: "3,300원",
     },
     {
       image: "./sub2/images/content2/new4.jpg",
       productName: "미노리키친 가츠동과 삼각김밥 ",
       company: "7-ELEVEn",
-      description: "일본 가정식의 맛 그대로 미노리키친 푸드 시리즈 2탄",
-      mdReview: "오늘 한끼는 가츠동과 삼각김밥 어떠세요?",
+      description: "일본 가정식의 맛 그대로 미노리키친 푸드 시리즈",
+      mdReview: "오늘 한끼는 가츠동과 삼각김밥 어때요?",
       price: "가츠동 5,800원 / 삼각 1,300원",
     },
     {
@@ -37,22 +37,22 @@ $(document).ready(function () {
       productName: "롯샌 멜론",
       company: "LOTTE",
       description: "요즘 대세 인기 멜론 맛으로 역대급 멜론 과자",
-      mdReview:"🍈롯샌 멜론과 함께 멜론 코어에 본격 탑승!",
+      mdReview:"🍈롯샌 멜론과 함께 멜론 코어에 탑승!",
       price: "1,700원",
     },
     {
       image: "./sub2/images/content2/new6.jpg",
-      productName: "부르봉블란츌딸기랑그드샤",
+      productName: "부르봉블란츌 딸기 랑그드샤",
       company: "7-ELEVEn",
-      description: "입에서 사르르 순삭하는 부르봉블란츌 딸기 랑그드샤",
-      mdReview: "부르봉블란츌 딸기 랑그드샤..샤랑해…💕 ",
+      description: "입에서 사르르 순삭하는 딸기 랑그드샤",
+      mdReview: " 딸기 랑그드샤..샤랑해…💕 ",
       price: "3,000원",
     },
     {
       image: "./sub2/images/content2/new7.jpg",
       productName: "스윙칩고수타코맛 ",
       company: "오리온",
-      description: "고수 입문자도, 고수 러버도 맛있게 즐길 수 있는 신상",
+      description: "고수 입문자도, 고수 러버도 맛있게 즐길 수 있어요",
       mdReview: "과연 나의 고수력 은?!🧐",
       price: "1,700원",
     },
@@ -68,8 +68,8 @@ $(document).ready(function () {
       image: "./sub2/images/content2/new9.jpg",
       productName: "딸기이즘콘",
       company: "LOTTE",
-      description: "딸기우유맛에 치즈다이스가 더해져 달콤하고 고소해요!",
-      mdReview: "딸기이즘콘 먹으러 세븐 갈 사람! ദ്ദി(｡•̀ ,<)~✩‧₊",
+      description: "본연의 딸기우유맛에 치즈다이스가 더해져 달콤하고 고소해요!",
+      mdReview: "딸기이즘콘 먹으러 세븐 갈 사람!",
       price: "3,000원",
     },
 
@@ -78,7 +78,7 @@ $(document).ready(function () {
       productName: "머그컵 카스테라볼 , 메이플 시럽 팬케익 ",
       company: "7-ELEVEn",
       description: "🤎카라멜 밀크크림과 🍯메이플시럽을 준비했어요",
-      mdReview: "달달한 간식 땡기는 사람, 여기 다 모여라 🙋‍♀️",
+      mdReview: "달달한 간식은 사랑입니다. ",
       price: "카스테라볼 3,400원 / 팬케익 2,800원 ",
     },
   ];
@@ -89,19 +89,37 @@ $(document).ready(function () {
   let timer;
 
   // 특정번호 슬라이드 내용 화면에 보여주는 함수
+
   function showSlide(index) {
     const currentData = newMenuData[index];
+    const $contentWrapper = $(".new_menu_content_wrapper");
+    // 애니매이션이 진행중이면 또 실행되지 않도록 함.
+    if ($contentWrapper.is(':animated')) {
+      return;
+    }
 
-    // attr() 속성값을 가져오거나 변경할수 있는 함수.
-    $(".new_menu_right_box img").attr("src", currentData.image);
+    $contentWrapper.animate({ opacity: 0, left: "-100px" }, 400, function () {
+      const newImg = new Image();
+      newImg.src = currentData.image;
 
-    const $menuChange = $(".new_menu_change");
-    $menuChange.find("dd").eq(0).text(currentData.productName);
-    $menuChange.find("dd").eq(1).text(currentData.company);
-    $menuChange.find("dd").eq(2).text(currentData.description);
-    $menuChange.find("dd").eq(3).find("span").text(currentData.mdReview);
-    $menuChange.find("dd").eq(4).find("span").text(currentData.price);
+      // 이미지 로드가 완료되면 실행됨.
+      newImg.onload = function () {
+        // attr() 속성값을 가져오거나 변경할수 있는 함수.
+        $contentWrapper.find(".new_menu_right_box img").attr("src", newImg.src);
+        
+        const $menuChange = $contentWrapper.find(".new_menu_change");
 
+        $menuChange.find("dd").eq(0).text(currentData.productName);
+        $menuChange.find("dd").eq(1).text(currentData.company);
+        $menuChange.find("dd").eq(2).text(currentData.description);
+        $menuChange.find("dd").eq(3).find("span").text(currentData.mdReview);
+        $menuChange.find("dd").eq(4).find("span").text(currentData.price);
+      
+        $contentWrapper.css("left", "100px");
+        $contentWrapper.animate({ opacity: 1, left: "0px" }, 400);
+      }
+    });
+        
     // 먼저 초기화
     $(".new_menu_dot").removeClass("active");
     //추가
@@ -110,9 +128,11 @@ $(document).ready(function () {
     currentIndex = index;
   }
     //클릭 기능
-    $(".new_menu_dot").on("click", function () {
+    $(".new_menu_slider_dots").on("click",".new_menu_dot", function () {
       const index = $(this).index();
+      clearInterval(timer)
       showSlide(index);
+      startSlider();
     });
 
   function startSlider() {
@@ -120,7 +140,7 @@ $(document).ready(function () {
     timer = setInterval(function () {
       let nextIndex = (currentIndex + 1) % newMenuData.length;
       showSlide(nextIndex);
-    }, 2000);
+    }, 3000);
   }
 
   $(document).ready(function () {
