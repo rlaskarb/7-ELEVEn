ffa32d344303c4d01af70cf0a3267bc58eac9537
rlaskarb <rlaskarb20@gmail.com>
2025-08-01 23:56:04 +0900

    8/1

--- www/sub4/js/sub4_2_procedures.js
+++ www/sub4/js/sub4_2_procedures.js
@@ -1,29 +1,40 @@
-$(document).ready(function () {
-	const $header = $("#headerArea");
-	const $proceduresNav = $(".procedures_nav");
-	const $navLinks = $proceduresNav.find("a");
-	const $descroptionItem = $(".description_item");
-	const stickyNavTop = $proceduresNav.offset().top;
-	const navHeight = $proceduresNav.outerHeight();
+// $(document).ready(function () {
+// 	const $header = $("#headerArea");
+// 	const $proceduresNav = $(".procedures_nav");
+// 	const $navLinks = $proceduresNav.find("a");
+// 	const $descriptionItem = $(".description_item");
+// 	const stickyNavTop = $proceduresNav.offset().top;
+// 	const navHeight = $proceduresNav.outerHeight();
 
-	$(window).on("scroll", function () {
-		const scrollTop = $(this).scrollTop();
-		if (scrollTop >= stickyNavTop) {
-			$header.hide();
-			$proceduresNav.addClass("fixed");
-        } else {
-            $header.show();
-            $proceduresNav.removeClass("fixed");    
-        }
+// 	$(window).on("scroll", function () {
+// 		const scrollTop = $(this).scrollTop();
+// 		if (scrollTop >= stickyNavTop) {
+// 			$header.hide();
+// 			$proceduresNav.addClass("fixed");
+// 		} else {
+// 			$header.show();
+// 			$proceduresNav.removeClass("fixed");
+// 		}
 
-        $descroptionItem.each(function (index) {
-            const itemTop = $(this).offset().top;
+// 		$descriptionItem.each(function (index) {
+// 			const itemTop = $(this).offset().top;
 
-            if (scrollTop >= itemTop - navHeight - 100) {
-                $navLinks.parent().removeClass('on')
-                
-            }
-            
-        })
-	});
-});
+// 			if (scrollTop >= itemTop - navHeight - 100) {
+// 				$navLinks.parent().removeClass("on");
+// 				$navLinks.eq(index).parent().addClass("on");
+// 			}
+// 		});
+// 	});
+
+// 	$navLinks.on("click", function (event) {
+// 		event.preventDefault();
+
+// 		const targetSelector = $(this).data(target);
+// 		const $targetElement = $(targetSelector);
+
+// 		if ($targetElement.length) {
+// 			const offsetPosition = $targetElement.offset().top - navHeight - 100;
+// 			$("html,body").animate({ scrollTop: offsetPosition }, 1000);
+// 		}
+// 	});
+// });
